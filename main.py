89b117c3099bfe6d877a67aabd0ec47245cefda0
Istoc42 <shudso91@gmail.com>
2023-03-05 13:15:15 +0000

    Wrote readme

--- main.py
+++ main.py
@@ -3,38 +3,45 @@
 import re
 import random
 
+# FUNCTIONS
+def standard_roll(user_input):
+    # Take the string apart and split into list of integers
+    roll_input_list = user_input.split('d')
+    # print(roll_input_list)
+
+    num_of_dice = int(roll_input_list[0])
+    num_of_sides = int(roll_input_list[1])
+
+    # print(f'Number of dice: {num_of_dice}')
+    # print(f'Number of sides: {num_of_sides}')
+
+    # Generate random number with num_of_sides as the maximum value a number of times equal to num_of_dice
+    i = 0
+    dice_rolls = []
+    while i != num_of_dice:
+        i += 1
+        roll = random.randint(1, num_of_sides)
+        dice_rolls.append(roll)
+        # print(f'Roll {i}: {roll}')
+
+    # Print dice_rolls and total of all values
+    print(f'Total: {sum(dice_rolls)} {dice_rolls}')
+
+# USER INPUT VALIDATION
 while True:
     # Take input from user as dice roll string(e.g. 2d12)
-    roll_input = input("Enter your dice roll: ").lower()
+    roll_input = input("Enter your dice roll or 'help': ").lower()
     # print(roll_input)
 
     # Check that the string has a "d" in between 2 numbers
     regex = f'[0-9]+d[0-9]+'
-    if re.search(regex, roll_input):
+    if roll_input == 'help':
+        print('Work in progress. Help menu goes here...')
+    elif re.search(regex, roll_input):
         # print("Regex passed. Move on to rng.")
+        standard_roll(roll_input)
         break
     else:
         print("Invalid entry. Please enter a valid dice roll (e.g. 3d6)")
         continue
 
-# Take the string apart and split into list of integers
-roll_input_list = roll_input.split('d')
-# print(roll_input_list)
-
-num_of_dice = int(roll_input_list[0])
-num_of_sides = int(roll_input_list[1])
-
-# print(f'Number of dice: {num_of_dice}')
-# print(f'Number of sides: {num_of_sides}')
-
-# Generate random number with num_of_sides as the maximum value a number of times equal to num_of_dice
-i = 0
-dice_rolls = []
-while i != num_of_dice:
-    i += 1
-    roll = random.randint(1, num_of_sides)
-    dice_rolls.append(roll)
-    # print(f'Roll {i}: {roll}')
-
-# Print dice_rolls and total of all values
-print(f'Total: {sum(dice_rolls)} {dice_rolls}')
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Dice Roller by Shaun Hudson
+
+Features:
+- Generate basic dice rolls (E.g. 1d20, 3d6, 12d8)
+
+To do:
+- Allow for plus and minus modifiers (e.g. 1d20+5, 3d6+2, 2d20-5)
+- Calculate average and print along side result
+- Function to roll for stats
+- Help menu
