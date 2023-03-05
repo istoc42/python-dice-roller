7e68d584c40f70016ce6954ec757502822a5a83f
Istoc42 <shudso91@gmail.com>
2023-03-05 13:05:05 +0000

    Commented out debug prints

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ import random
 while True:
     # Take input from user as dice roll string(e.g. 2d12)
     roll_input = input("Enter your dice roll: ").lower()
-    print(roll_input)
+    # print(roll_input)
 
     # Check that the string has a "d" in between 2 numbers
     regex = f'[0-9]+d[0-9]+'
     if re.search(regex, roll_input):
-        print("Regex passed. Move on to rng.")
+        # print("Regex passed. Move on to rng.")
         break
     else:
-        print("Invalid entry. Loop back to input")
+        print("Invalid entry. Please enter a valid dice roll (e.g. 3d6)")
         continue
 
 # Take the string apart and split into list of integers
 roll_input_list = roll_input.split('d')
-print(roll_input_list)
+# print(roll_input_list)
 
 num_of_dice = int(roll_input_list[0])
 num_of_sides = int(roll_input_list[1])
 
-print(f'Number of dice: {num_of_dice}')
-print(f'Number of sides: {num_of_sides}')
+# print(f'Number of dice: {num_of_dice}')
+# print(f'Number of sides: {num_of_sides}')
 
 # Generate random number with num_of_sides as the maximum value a number of times equal to num_of_dice
 i = 0
@@ -34,7 +34,7 @@ while i != num_of_dice:
     i += 1
     roll = random.randint(1, num_of_sides)
     dice_rolls.append(roll)
-    print(f'Roll {i}: {roll}')
+    # print(f'Roll {i}: {roll}')
 
 # Print dice_rolls and total of all values
 print(f'Total: {sum(dice_rolls)} {dice_rolls}')
