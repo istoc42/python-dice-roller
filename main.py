659f51b8f55bdf2afed6901689c02da0cc8a7199
Istoc42 <shudso91@gmail.com>
2023-03-05 14:01:37 +0000

    Added modifier function for plus and minus

--- main.py
+++ main.py
@@ -7,13 +7,9 @@ import random
 def standard_roll(user_input):
     # Take the string apart and split into list of integers
     roll_input_list = user_input.split('d')
-    # print(roll_input_list)
 
     num_of_dice = int(roll_input_list[0])
     num_of_sides = int(roll_input_list[1])
-
-    # print(f'Number of dice: {num_of_dice}')
-    # print(f'Number of sides: {num_of_sides}')
 
     # Generate random number with num_of_sides as the maximum value a number of times equal to num_of_dice
     i = 0
@@ -22,26 +18,61 @@ def standard_roll(user_input):
         i += 1
         roll = random.randint(1, num_of_sides)
         dice_rolls.append(roll)
-        # print(f'Roll {i}: {roll}')
 
     # Print dice_rolls and total of all values
     print(f'Total: {sum(dice_rolls)} {dice_rolls}')
+
+def modified_roll(user_input):
+    # Split the input into parts
+    regex = f'(\d+)|(\+)|(\-)'
+    roll_input = re.findall(regex, user_input)
+
+    # Tidy up roll_input
+    roll_input = [item for sublist in roll_input for item in sublist if item]
+
+    # TODO: Detect what type of operator is at the end of the roll
+    num_of_dice = int(roll_input[0])
+    num_of_sides = int(roll_input[1])
+    operator = roll_input[2]
+    modifier = int(roll_input[3])
+    total = 0
+
+    # Generate random number with num_of_sides as the maximum value a number of times equal to num_of_dice
+    i = 0
+    dice_rolls = []
+    while i != num_of_dice:
+        i += 1
+        roll = random.randint(1, num_of_sides)
+        dice_rolls.append(roll)
+
+    total = sum(dice_rolls)
+
+    # Detect what type of operator is used and apply it to total roll
+    if operator == '+':
+        total += modifier
+    else:
+        total -= modifier
+
+    # Return total including modifier
+    print(f'Total: {total} {dice_rolls} {operator} {modifier}')
+
 
 # USER INPUT VALIDATION
 while True:
     # Take input from user as dice roll string(e.g. 2d12)
     roll_input = input("Enter your dice roll or 'help': ").lower()
-    # print(roll_input)
 
     # Check that the string has a "d" in between 2 numbers
-    regex = f'[0-9]+d[0-9]+'
+    standard_roll_regex = f'^[0-9]+d[0-9]+$' # Checks for a number, followed by a 'd',  followed by another number
+    modified_roll_regex = f'^[0-9]+d[0-9]+[+\-*\/][0-9]+$' # Check for the same as above but followed by a mathematical operator
+
     if roll_input == 'help':
         print('Work in progress. Help menu goes here...')
-    elif re.search(regex, roll_input):
-        # print("Regex passed. Move on to rng.")
+    elif re.search(standard_roll_regex, roll_input):
         standard_roll(roll_input)
-        break
+    elif re.search(modified_roll_regex, roll_input):
+        modified_roll(roll_input)
     else:
-        print("Invalid entry. Please enter a valid dice roll (e.g. 3d6)")
+        print("Invalid entry. Try 'help'")
         continue
 
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@ Dice Roller by Shaun Hudson
 
 Features:
 - Generate basic dice rolls (E.g. 1d20, 3d6, 12d8)
+- Allow for plus and minus modifiers (e.g. 1d20+5, 3d6+2, 2d20-5)
 
 To do:
-- Allow for plus and minus modifiers (e.g. 1d20+5, 3d6+2, 2d20-5)
 - Calculate average and print along side result
 - Function to roll for stats
 - Help menu
